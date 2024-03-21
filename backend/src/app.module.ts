5d0fb9071b1bc3adc82c00942685a9e496875f16
Exuper O <exuper.dev@gmail.com>
2024-03-21 15:29:50 +0100

    chore(files): cleanup backend app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [AuthModule, UsersModule, RecipesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
